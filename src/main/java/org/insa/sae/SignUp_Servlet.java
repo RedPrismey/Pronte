c0dec3aea61592ee0773df30a297319ff3a8086d
Prismey <prismey@murena.io>
2026-01-23 09:56:24 +0100

    hash password

--- src/main/java/org/insa/sae/SignUp_Servlet.java
+++ src/main/java/org/insa/sae/SignUp_Servlet.java
@@ -10,23 +10,18 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/SignUp_Servlet")
 public class SignUp_Servlet extends HttpServlet {
-    
-    
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/pronte";
-    private static final String DB_USER = "root";
-    private static final String DB_PWD = ""; 
-
     @Override
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     request.setCharacterEncoding("UTF-8");
     
     try {
-        // 1. Récupération des données
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
         String nom = request.getParameter("nom");
         String prenom = request.getParameter("prenom");
-        String roleStr = request.getParameter("role"); // "Etudiant", "Enseignant", "Admin"
+        String roleStr = request.getParameter("role");
         String username = request.getParameter("username");
-        String mdp = request.getParameter("mdp");
+        String mdp = digest.digest(request.getParameter("mdp").getBytes(StandardCharsets.UTF_8));;
 
         
         Role role = switch(roleStr) {
@@ -38,7 +33,7 @@ protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 
         
         User user = new User(0, username, mdp, role, nom, prenom);
-        user.save(); // 
+        user.save();
 
       
         response.sendRedirect("login.jsp?success=1");
